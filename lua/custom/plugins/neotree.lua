70654c13d04b2b06f908b2a209c05530ee4c9408
meskedal <marius.eskedal@gmail.com>
2024-01-26 23:52:53 +0100

    Updated neotree toggle

--- lua/custom/plugins/neotree.lua
+++ lua/custom/plugins/neotree.lua
@@ -1,5 +1,7 @@
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
+vim.api.nvim_set_keymap('n', '<leader>tt', '<Cmd>Neotree toggle<CR>', { noremap = true, silent = true, desc="toggle Neo[t]ree" })
+
 return {
   "nvim-neo-tree/neo-tree.nvim",
   version = "*",
@@ -9,6 +11,15 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function ()
-    require('neo-tree').setup {}
+    require('neo-tree').setup {
+      update_focused_file = {
+        enable = true,
+      },
+     -- window = {
+     --    mappings = {
+     --      ["<space>"] = {'none'}
+     --    }
+     --  }
+    }
   end,
 }
