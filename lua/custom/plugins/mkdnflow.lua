33f62b15d54aa9fdfda0753907bdd4f6388ac29c
meskedal <marius.eskedal@gmail.com>
2024-01-29 19:43:42 +0100

    Allowing copilot in md files

--- lua/custom/plugins/mkdnflow.lua
+++ lua/custom/plugins/mkdnflow.lua
@@ -4,9 +4,12 @@ return {
   'jakewvincent/mkdnflow.nvim',
   config = function()
       require('mkdnflow').setup({
-        -- Config goes here; leave blank for defaults
         mappings = {
-          MkdnToggleToDo = {{'n', 'v'}, '<C-<leader>>'},
+          MkdnToggleToDo = {{'n', 'v'}, '<leader>tm'},
+          -- MkdnNextLink = false,
+          -- MkdnPrevLink = false,
+          MkdnTableNextCell = false, -- Disabled because it conflicts with copilot tab complete in insert mode
+          MkdnTablePrevCell = false, -- Disabled because it conflicts with copilot tab complete in insert mode 
         }
       })
   end
